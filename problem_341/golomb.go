54001c59d2959203452b167db4bdf23723306f36
Dave Grijalva <grijalva@gmail.com>
2011-07-17 15:19:47 -0700

    getting somewhere

--- problem_341/golomb.go
+++ problem_341/golomb.go
@@ -1,5 +1,7 @@
 package golomb
 
+import "fmt"
+
 type Entry struct {
 	N uint64
 	GN uint64
@@ -31,9 +33,24 @@ func StartGenerator()(c chan Entry) {
 }
 
 func startGenerator(c chan Entry) {
-	var i uint64
+	var n, gn, gnr, gnn uint64
+	n = 1
+	gn = 1
+	gnr = 0
+	gnn = 1
 	for {
-		i++
-		c <- Entry{i,1}
+		if n < 10 {
+			fmt.Printf("step\t%v\t%v\t%v\t%v\n", n, gn, gnr, gnn)
+		}
+		c <- Entry{n,gn}
+		
+		n++
+		if gnr > 0 {
+			gnr--
+		} else {
+			gn++
+			gnr = gnn
+			gnn = n
+		}
 	}
 }
