776f7e347bea0d0afdbce607b03ca97973e415a4
Dave Grijalva <grijalva@gmail.com>
2011-07-17 13:44:54 -0700

    starting with problem 341 (like a boss)

---
+++ problem_341/main.go
@@ -0,0 +1,15 @@
+package main
+
+// The Golomb's self-describing sequence {G(n)} is the only nondecreasing sequence of 
+// natural numbers such that n appears exactly G(n) times in the sequence. 
+// The values of G(n) for the first few n are
+// 
+// n		1	2	3	4	5	6	7	8	9	10	11	12	13	14	15	…
+// G(n)	1	2	2	3	3	4	4	4	5	5	5	6	6	6	6	…
+// 
+// You are given that G(103) = 86, G(106) = 6137.
+// You are also given that ΣG(n3) = 153506976 for 1  n  103.
+// 
+// Find ΣG(n3) for 1  n  106.
+// 
+
